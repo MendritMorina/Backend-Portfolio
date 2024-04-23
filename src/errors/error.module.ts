c257c67d7d1f3f35e76935b09d528a90b637e7d7
Mendrit Morina <mendritmorina4@gmail.com>
2024-04-23 16:30:58 +0200

    I set user at error module beacuase user is a provider

--- src/errors/error.module.ts
+++ src/errors/error.module.ts
@@ -3,9 +3,10 @@ import {TypeOrmModule} from '@nestjs/typeorm';
 import {ErrorLog} from "./entities/error.entity";
 import {ErrorLogController} from "./error.controller";
 import {ErrorLogService} from "./error.service";
+import {User} from "../users/entities/user.entity";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([ErrorLog])],
+    imports: [TypeOrmModule.forFeature([ErrorLog,User])],
     controllers: [ErrorLogController],
     providers: [ErrorLogService],
 })
