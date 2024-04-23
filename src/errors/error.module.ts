d286bc5a135712c2d08b71c270afd5d70381c378
Mendrit Morina <mendritmorina4@gmail.com>
2024-04-23 16:35:55 +0200

    ConfigService at error module

--- src/errors/error.module.ts
+++ src/errors/error.module.ts
@@ -4,11 +4,12 @@ import {ErrorLog} from "./entities/error.entity";
 import {ErrorLogController} from "./error.controller";
 import {ErrorLogService} from "./error.service";
 import {User} from "../users/entities/user.entity";
+import {ConfigService} from "@nestjs/config";
 
 @Module({
     imports: [TypeOrmModule.forFeature([ErrorLog,User])],
     controllers: [ErrorLogController],
-    providers: [ErrorLogService],
+    providers: [ErrorLogService,ConfigService],
 })
 export class ErrorLogModule {
 }
